d85d8d5ea806b7ed71d2c6fbcbe9f3ab5e58db2d
AlfredBryan <alfred.chimereze@gmail.com>
2019-04-08 18:54:35 +0100

    Add Schema

---
+++ src/models/Settings/Settings.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const settingSchema = new mongoose.Schema({
+  app_name: {
+    type: String,
+    required: true
+  },
+
+  app_version: {
+    type: String,
+    required: true
+  }
+});
+
+const Setting = mongoose.model("Setting", settingSchema);
+
+module.exports = Setting;
