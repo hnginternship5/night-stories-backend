5166433a3e6f4ed1ed45f5128cec4d4f587fdff0
olaysco <olayiwolaodunsi@gmail.com>
2019-04-09 23:54:04 +0100

    implemented remove for bookmark, and corrected some hidden errs

--- src/modules/Bookmark/controllers/index.js
+++ src/modules/Bookmark/controllers/index.js
@@ -15,13 +15,17 @@ module.exports.getBookmark = async (req, res) => {
 };
 
 module.exports.addBookmark = async (req, res) => {
-    //this assumes the passed user and story params are valid ids of each field
     const { user, story} = req.body;
+    let validObject = mongoose.Types.ObjectId;
+    //this ensures user id and story id passed are both valid objectId types
+    if (!validObject.isValid(user)|| !validObject.isValid(story)) {
+        sendJSONResponse(res, 200, Error({ status: 422 }), req.method, 'Invalid user or story id');
+    }else{ 
     //check if the story has been bookmarked by the user
     Bookmark.findOne({ user: user, story:story }).then(bookmark => {
         if(bookmark){
-            return sendJSONResponse(res, 409, {}, req.method, 'Already Bookmarked');
-            //if it hasnt create a new bookmark
+            return sendJSONResponse(res, 200, {}, req.method, 'Already Bookmarked');
+            //if it hasnt, create a new bookmark
         }else{
             var newBookmark = new Bookmark();
             newBookmark.user = user;
@@ -34,5 +38,32 @@ module.exports.addBookmark = async (req, res) => {
             });
         }
     });
-        
+    }    
+}
+
+module.exports.delBookmark = async (req, res) => {
+     let validObject = mongoose.Types.ObjectId;
+     //this ensures user id and story id passed are both valid objectId types
+    if (!validObject.isValid(req.params.b_id) || !validObject.isValid(req.params.u_id)) {
+        sendJSONResponse(res, 200, Error({ status: 422 }), req.method, 'Invalid bookmark or user Id');
+    }else{
+        //try to find the book with the supplied ids before deleting
+        Bookmark.findOne({_id:req.params.b_id, user:req.params.u_id}).then(bookmark => {
+            //if bookmark not found, then exit with error message
+            if(!bookmark){
+                sendJSONResponse(res, 200, {}, req.method, 'bookmark doesn\'t exist');
+                //else attempt to delete the bookmark
+            }else{
+                Bookmark.deleteOne({_id:req.params.b_id, user:req.params.u_id},
+                function(err, bookmark){
+                    if(!err){
+                        sendJSONResponse(res, 200, {}, req.method, 'bookmark deleted successfully');
+                    }else{
+                        sendJSONResponse(res, 200, {bookmark}, req.method, 'Bookmark doesnt exist!');
+                    }
+                });
+            }
+        });
+
+    }
 }
