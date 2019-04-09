9b5acf7101b81c3b051936ff2afb692e52bbb45f
Aigbefo Ephraim <aigbefoephraim@yahoo.com>
2019-04-09 07:42:07 +0100

    Refactored user registration to prevent signup with same email

--- src/modules/User/policies/index.js
+++ src/modules/User/policies/index.js
@@ -6,6 +6,17 @@ module.exports.register = {
     name: joi.string().required(),
     password: joi.string().alphanum().min(3).max(30)
       .required(),
+    designation: joi.string().required(),
+    is_admin: joi.boolean(),
+    is_premium: joi.boolean()
+  },
+};
+
+module.exports.login = {
+  body: {
+    email: joi.string().email().required(),
+    password: joi.string().alphanum().min(3).max(30)
+      .required(),
   },
 };
 
