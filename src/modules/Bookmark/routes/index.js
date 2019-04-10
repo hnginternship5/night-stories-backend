824b03848eb31f77dcf21894076aabc619ffe366
Lord Jerry <lovinjerry004@gmail.com>
2019-04-10 10:49:14 +0100

    refactor codebase to follow eslint styling

--- src/modules/Bookmark/routes/index.js
+++ src/modules/Bookmark/routes/index.js
@@ -3,8 +3,8 @@ const ctrlBookmark = require('../controllers/index');
 
 const router = express.Router();
 
-//sample request looks like this http://localhost:3000/api/v1/bookmark/5cacd5567bba342dbc4ea002
+// sample request looks like this http://localhost:3000/api/v1/bookmark/5cacd5567bba342dbc4ea002
 router.get('/:id', ctrlBookmark.getBookmark);
 router.post('/add', ctrlBookmark.addBookmark);
-router.delete('/delete/bookmark_id/:b_id/user_id/:u_id', ctrlBookmark.delBookmark)
+router.delete('/delete/bookmark_id/:b_id/user_id/:u_id', ctrlBookmark.delBookmark);
 module.exports = router;
