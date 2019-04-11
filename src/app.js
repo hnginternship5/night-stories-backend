233a9650298813dd9036d56a2b5b08d3a99bf8a7
EphraimDev <aigbefoephraim@yahoo.com>
2019-04-11 13:37:33 +0100

    Add cors

--- src/app.js
+++ src/app.js
@@ -21,6 +21,14 @@ if (config.env !== 'test') {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: '52428800' }));
 app.use(cookieParser());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json, Authorization');
+  next();
+});
 const apiRoutes = require('./router');
 
 app.use('/api/v1', apiRoutes);
